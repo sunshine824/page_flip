bf21cd90b7926d773536e656084c58f09bd409da
sunshine824 <790168710@qq.com>
2018-10-11 18:34:35 +0800

    滑动效果

--- src/main.js
+++ src/main.js
@@ -9,10 +9,20 @@ Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
 const history = window.sessionStorage
+let count = history.getItem('count') || 0;
+history.clear()
+history.setItem('/', 0)
 
 router.beforeEach((to, from, next) => {
-    console.log(to)
-    console.log(from)
+    let toIndex = history.getItem(to.path)
+    let prevIndex = history.getItem(from.path)
+
+    if (toIndex) {
+
+    } else {
+        ++count;
+        history.setItem(to.path, count)
+    }
     next();
 })
 
