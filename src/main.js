783711625f38085a5192136f43a941605b2cfb5a
sunshine824 <790168710@qq.com>
2018-10-12 15:10:00 +0800

    将接口api放在vuex中

--- src/main.js
+++ src/main.js
@@ -8,11 +8,20 @@ Vue.config.productionTip = false
 
 Vue.prototype.$http = axios
 
+//http request拦截器
+axios.interceptors.response.use((config)=>{
+    console.log(config)
+    return config
+},(err)=>{
+    return Promise.reject(err);
+})
+
 const history = window.sessionStorage
 let count = history.getItem('count') || 0;
 history.clear()
 
 router.beforeEach((to, from, next) => {
+    //第一级栏目
     const subArr = ['/', '/about', '/setting']
     let [toIndex, prevIndex] = [parseInt(history.getItem(to.path)), parseInt(history.getItem(from.path))]
     for (let index in subArr) history.setItem(subArr[index], 0)
@@ -20,7 +29,7 @@ router.beforeEach((to, from, next) => {
     if (toIndex || toIndex === 0) {
         if (toIndex > prevIndex) {
             store.dispatch('set_direction_state', 'next')
-        } else if (toIndex === prevIndex) {
+        } else if (toIndex === prevIndex) {  //一级栏目无切换效果
             store.dispatch('set_direction_state', '')
         } else {
             store.dispatch('set_direction_state', 'prev')
